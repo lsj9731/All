43ba8014115f74a12a486acd9170bf8bea9a31b2
lsj9731 <81149234+lsj9731@users.noreply.github.com>
2022-03-29 11:00:30 +0900

    Delete Projects/THUMOS2014 - 2021_06 directory

--- Projects/THUMOS2014 - 2021_06/PGCN_2021/test_two_stream.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python eval_detection_results.py sleep results/flow_sleep_result results/rgb_sleep_result --score_weights 1.2 1 --nms_threshold 0.32
--- Projects/THUMOS2014 - 2021_06/PGCN_2021/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-save_path=$1
-
-python pgcn_train.py thumos14 --snapshot_pre ${save_path} 
